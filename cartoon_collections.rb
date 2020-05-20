8aa323a28ac3778408e1d7a53e849c3de8ecd1a7
Julio Chazari <julio.chazari@gmail.com>
2020-05-20 23:45:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,4 +27,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find { |string| string == valid_calls }
+  
 end
